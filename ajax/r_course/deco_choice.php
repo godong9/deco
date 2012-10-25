63a12617cf350379e4828cb6c497c471fb60344c
soma <soma@ubuntu62.(none)>
2012-10-25 17:32:19 +0900

    user_recommend, deco_choice, deco_event implementation

--- ajax/r_course/deco_choice.php
+++ ajax/r_course/deco_choice.php
@@ -1,1 +1,10 @@
-tets
+<?
+	require_once( $_SERVER['DOCUMENT_ROOT'] . "/lib/r_course/deco_choice.inc");
+
+	$DECOCHOICE = new DECOCHOICE();
+	$DECOCHOICE->init(true);
+
+	$params = array();
+	$array = $DECOCHOICE->getDecoChoice($params);
+	echo json_encode($array);
+?>
--- ajax/r_course/user_recommend.php
+++ ajax/r_course/user_recommend.php
@@ -1,1 +1,10 @@
-tes
+<?
+	require_once( $_SERVER['DOCUMENT_ROOT'] . "/lib/r_course/user_recommend.inc");
+
+	$USERRECOMMEND = new USERRECOMMEND();
+	$USERRECOMMEND->init(true);
+
+	$params = array();
+	$array = $USERRECOMMEND->getUserRecommend($params);
+	echo json_encode($array);
+?>
